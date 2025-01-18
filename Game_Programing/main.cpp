f467965adc4e1c329c00b7e2a32e5cfb7d15994d
dbswogus <jacob30565@gmail.com>
2025-01-18 19:11:42 +0900

    여러개의 사각형이 벽을 찍으면 반대로 돌아감

--- Game_Programing/main.cpp
+++ Game_Programing/main.cpp
@@ -2,21 +2,29 @@
 #include <iostream>
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!");
-
-	int move_pos_rand_x = rand() % 1920;
-	int move_pos_rand_y = rand() % 1080;
+	const int windowwidth = 1920;
+	const int windowheigh = 1080;
+	sf::RenderWindow window(sf::VideoMode(windowwidth, windowheigh), "SFML works!");
+	
 	const int rect_num = 30;
 	sf::RectangleShape rect[rect_num];
+
+	float move[rect_num];
 	for (int i = 0; i < rect_num;i++)
 	{
-		int rand_x = rand() % 1920;
-		int rand_y = rand() % 1080;
 		const int size_x = 50;
 		const int size_y = 50;
+		int rand_x = rand() % 1920;
+		int rand_y = rand() % 1080;
+		unsigned char rand_colorR = rand() % 255;
+		unsigned char rand_colorG = rand() % 255;
+		unsigned char rand_colorB = rand() % 255;
+		unsigned char rand_color = rand() % 255;
+
 		rect[i].sf::Transformable::setPosition(sf::Vector2f(rand_x, rand_y));
 		rect[i].setSize(sf::Vector2f(size_x, size_y));
-		//rect[i].setFillColor(sf::Color(rand() % 255));
+		rect[i].setFillColor(sf::Color(rand_colorR,rand_colorG,rand_colorB, rand_color));
+		move[i] = rand()%2+0.5;
 	}
 	while (window.isOpen())
 	{
@@ -31,6 +39,18 @@ int main()
 		for (int i = 0; i < rect_num;i++)
 		{
 			window.draw(rect[i]);
+		}
+		for (int i = 0; i < rect_num;i++)
+		{
+			sf::Vector2f pos = rect[i].getPosition();
+			pos.x += move[i];
+			if (pos.x > windowwidth || pos.x < 0)
+			{
+				move[i] = -move[i];
+			}
+			rect[i].sf::Transformable::setPosition(pos.x,pos.y);
+			
+
 		}
 		window.display();
 	}
