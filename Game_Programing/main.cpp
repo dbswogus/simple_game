b0abe10527756ad2260b8faf623db192b5f6177b
dbswogus <jacob30565@gmail.com>
2025-01-18 16:24:37 +0900

    네모 찍기

--- Game_Programing/main.cpp
+++ Game_Programing/main.cpp
@@ -3,19 +3,21 @@
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!");
-	
-	sf::RectangleShape shape(sf::Vector2f(50,50));
-	float move_pos = 1920;
-	float move_pos_y = 0;
-	float move_direct = -1.0f;
-	shape.setFillColor(sf::Color::Green);
-	
-	sf::CircleShape circle(25.0f);
-	circle.setFillColor(sf::Color::Red);
-	float move_pos_c = 0;
-	float move_pos_c_y = 0;
-	float move_direct_c = 1.0f;
 
+	int move_pos_rand_x = rand() % 1920;
+	int move_pos_rand_y = rand() % 1080;
+	const int rect_num = 30;
+	sf::RectangleShape rect[rect_num];
+	for (int i = 0; i < rect_num;i++)
+	{
+		int rand_x = rand() % 1920;
+		int rand_y = rand() % 1080;
+		const int size_x = 50;
+		const int size_y = 50;
+		rect[i].sf::Transformable::setPosition(sf::Vector2f(rand_x, rand_y));
+		rect[i].setSize(sf::Vector2f(size_x, size_y));
+		//rect[i].setFillColor(sf::Color(rand() % 255));
+	}
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -24,23 +26,12 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-		shape.sf::Transformable::setPosition(sf::Vector2f(move_pos,move_pos_y));
-		circle.sf::Transformable::setPosition(sf::Vector2f(move_pos_c, move_pos_c_y));
-		move_pos += move_direct;
-		move_pos_c += move_direct_c;
-		if (move_pos == 0 || move_pos == 1920)
-		{
-			move_direct = -move_direct;
-			move_pos_y += 50;
-		}
-		if (move_pos_c == 0 || move_pos_c == 1920)
-		{
-			move_direct_c = -move_direct_c;
-			move_pos_c_y += 50;
-		}
+
 		window.clear();
-		window.draw(shape);
-		window.draw(circle);
+		for (int i = 0; i < rect_num;i++)
+		{
+			window.draw(rect[i]);
+		}
 		window.display();
 	}
 }
