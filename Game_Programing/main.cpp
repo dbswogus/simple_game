b6f9259a49caafff2e17093910037216a268e426
dbswogus <jacob30565@gmail.com>
2025-01-19 17:40:35 +0900

    플레이어가 움직이는 사각형

--- Game_Programing/main.cpp
+++ Game_Programing/main.cpp
@@ -2,30 +2,20 @@
 #include <iostream>
 int main()
 {
+	//window create
 	const int windowwidth = 1920;
 	const int windowheigh = 1080;
-	sf::RenderWindow window(sf::VideoMode(windowwidth, windowheigh), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(windowwidth, windowheigh), "Player move!");
 	
-	const int rect_num = 30;
-	sf::RectangleShape rect[rect_num];
+	//Player object create
+	sf::RectangleShape Player;
 
-	float move[rect_num];
-	for (int i = 0; i < rect_num;i++)
-	{
-		const int size_x = 50;
-		const int size_y = 50;
-		int rand_x = rand() % 1920;
-		int rand_y = rand() % 1080;
-		unsigned char rand_colorR = rand() % 255;
-		unsigned char rand_colorG = rand() % 255;
-		unsigned char rand_colorB = rand() % 255;
-		unsigned char rand_color = rand() % 255;
-
-		rect[i].sf::Transformable::setPosition(sf::Vector2f(rand_x, rand_y));
-		rect[i].setSize(sf::Vector2f(size_x, size_y));
-		rect[i].setFillColor(sf::Color(rand_colorR,rand_colorG,rand_colorB, rand_color));
-		move[i] = rand()%2+0.5;
-	}
+	//set player size and color
+	const float player_size_x = 30;
+	const float player_size_y = 30;
+	Player.setSize(sf::Vector2f(player_size_x, player_size_y));
+	Player.setFillColor(sf::Color::Red);
+	
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -35,22 +25,27 @@ int main()
 				window.close();
 		}
 
-		window.clear();
-		for (int i = 0; i < rect_num;i++)
+		window.clear(); 
+		window.draw(Player);
+		
+		//move logic
+		const float distance = 0.1f;
+		float speed = 5.0f;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 		{
-			window.draw(rect[i]);
+			Player.sf::Transformable::move(-distance*speed,0.0f);
 		}
-		for (int i = 0; i < rect_num;i++)
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 		{
-			sf::Vector2f pos = rect[i].getPosition();
-			pos.x += move[i];
-			if (pos.x > windowwidth || pos.x < 0)
-			{
-				move[i] = -move[i];
-			}
-			rect[i].sf::Transformable::setPosition(pos.x,pos.y);
-			
-
+			Player.sf::Transformable::move(distance*speed,0.0f);
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+		{
+			Player.sf::Transformable::move(0.0f,distance* speed);
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+		{
+			Player.sf::Transformable::move(0.0f,-distance* speed);
 		}
 		window.display();
 	}
